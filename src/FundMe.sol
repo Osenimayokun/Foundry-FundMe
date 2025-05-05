3277f660f89860af6b699faecb05984370dee67c
Oseni Mayokun <osenimayokun817@gmail.com>
2025-05-05 13:35:44 +0100

    made some modifications by using memory instead of storage

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -32,6 +32,24 @@ contract FundMe {
         s_amountSentFromFunders[msg.sender] = msg.value;
     }
 
+    function cheapWithdraw() public onlyOwner {
+        uint256 fundersLength = s_funders.length;
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < fundersLength;
+            funderIndex++
+        ) {
+            address funder = s_funders[funderIndex];
+            s_amountSentFromFunders[funder] = 0;
+        }
+
+        s_funders = new address[](0);
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call Failed");
+    }
+
     function withdraw() public onlyOwner {
         // require(msg.sender == owner, "Must be the Owner"); // Was ticked out because a modifer will be created
         for (
